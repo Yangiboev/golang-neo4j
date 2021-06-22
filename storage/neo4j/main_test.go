dd33b484b48bca99634cf15a6c59291807ce460c
Dilmurod Yangiboev <dilmurod.yangiboev@gmail.com>
2021-06-22 13:44:37 +0500

    responsible test added

---
+++ storage/neo4j/main_test.go
@@ -0,0 +1,39 @@
+package neo4j_test
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/Yangiboev/golang-neo4j/config"
+	"github.com/Yangiboev/golang-neo4j/pkg/logger"
+	"github.com/Yangiboev/golang-neo4j/storage"
+	"github.com/neo4j/neo4j-go-driver/v4/neo4j"
+)
+
+var (
+	err  error
+	log  logger.Logger
+	cfg  config.Config
+	strg storage.StorageI
+)
+
+func TestMain(m *testing.M) {
+	cfg = config.Load()
+	log = logger.New(cfg.LogLevel, "golang-neo4j")
+	log.Info("main: Neo4j connection config",
+		logger.String("Host", cfg.Neo4jHost),
+		logger.Int("Port", cfg.Neo4jPort),
+		logger.String("Database", cfg.Neo4jDatabase),
+	)
+	connUrl := fmt.Sprintf("neo4j://%s:%d", cfg.Neo4jHost, cfg.Neo4jPort)
+	neoDriver, err := neo4j.NewDriver(connUrl, neo4j.BasicAuth(cfg.Neo4jUser, cfg.Neo4jPassword, ""))
+	defer neoDriver.Close()
+	if err != nil {
+		log.Error("error to connect to neo4j", logger.Error(err))
+		panic(err)
+	}
+	log.Info("Connected to Neo4j", logger.Any("neo4j: ", cfg.Neo4jDatabase))
+	strg = storage.NewResponsibleStorage(neoDriver)
+	os.Exit(m.Run())
+}
---
+++ storage/neo4j/responsible_test.go
@@ -0,0 +1,41 @@
+package neo4j_test
+
+import (
+	"testing"
+
+	"github.com/Yangiboev/golang-neo4j/api/models"
+	"github.com/bxcodec/faker/v3"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+func createResponsible(t *testing.T) *models.Responsible {
+	var req *models.Responsible
+	err := faker.FakeData(&req)
+	assert.NoError(t, err)
+	ID, _ := uuid.NewRandom()
+	req.ID = ID.String()
+	response, err := strg.Responsible().Create(req)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, response)
+	return &models.Responsible{
+		ID:           req.ID,
+		Organization: req.Organization,
+		NameOfStep:   req.NameOfStep,
+		Role:         req.Role,
+		Comment:      req.Comment,
+		CreatedAt:    req.CreatedAt,
+		UpdatedAt:    req.UpdatedAt,
+	}
+}
+func TestCreateResponsible(t *testing.T) {
+	for i := 0; i < 1000; i++ {
+		_ = createResponsible(t)
+	}
+}
+func TestGetResponsible(t *testing.T) {
+	created := createResponsible(t)
+	response, err := strg.Responsible().Get(created.ID)
+	assert.NoError(t, err)
+	assert.Equal(t, created, response)
+}
